bede30ff1c72b70ea33996be0b1d29c13623aaf9
kittyneko <kittynekostudio@gmail.com>
2025-07-07 17:40:28 -0700

    First GET request

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,9 @@
-fn main() {}
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port.");
+    run(listener)?.await
+}
